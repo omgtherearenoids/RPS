9af4860ab40cee9b622652331a71546889cf4fbf
omgtherearenoids <istiaquerafi01@gmail.com>
2023-07-13 18:16:13 -0400

    Added UI

--- js/script.js
+++ js/script.js
@@ -56,39 +56,72 @@ function getResult(playerSelection1,computerSelection){
 
 }
 
-function game(){
+function getPlayerChoice(choice,playerScore,computerScore){
 
-    let playerScore = 0;
-    let computerScore = 0;
+    let playerChoice = choice; 
+    let comp = getComputerChoice();
+    let winVal = getResult(playerChoice,comp);
 
-
-    while(playerScore!=5 && computerScore!=5){
-
-        let playerChoice = prompt("ROCK, PAPER, or SCISSORS?!");
-        let comp = getComputerChoice();
-
-        let winVal = getResult(playerChoice,comp);
-
-        if(winVal == 1){
-            playerScore++;
-        }else if(winVal == 2){
-            computerScore++;
-        }else{
-            continue;
-        }
-    }
-
-    if(playerScore==5){
-        return "PLAYER WINS!";
+    if(winVal == 1){
+        const result = document.querySelector('#result');
+        result.textContent = "You won that Round!!";
+        updateResult(1);
+    }else if(winVal == 2){
+        const result = document.querySelector('#result');
+        result.textContent = "You lost that Round!";
+        updateResult(2);
     }else{
-        return "COMPUTER WINS!!!";
+        const result = document.querySelector('#result');
+        result.textContent = "tie!";
+        updateResult(3);
     }
 
+    
 
 }
 
+let playerScore = 0;
+let computerScore = 0;
+let roundTrack = 0;
 
-console.log(game())
+function updateResult(winBool){
+
+    
+    
+    roundTrack++;
+    const player = document.querySelector('#player');
+    const computer = document.querySelector('#comp');
+    const round = document.querySelector('#round');
+
+    if(winBool==1){
+        playerScore++;
+        player.textContent = "PLAYER SCORE: "+playerScore;
+        console.log(playerScore);
+    }else if(winBool==2){
+        computerScore++;
+        computer.textContent = "COMPUTER SCORE: "+computerScore;
+        console.log(computerScore);
+    }
+    
+    round.textContent = "ROUND "+roundTrack;
+
+    if(playerScore==5 ){
+        alert("YOU WON AGAINST THE COMPUTER!!");
+    }else if(computerScore==5){
+        alert("YOU LOST AGAINST THE COMPUTER!!");
+    }
+
+}
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+      getPlayerChoice(button.className,playerScore,computerScore);
+    });
+  });
+
+
+//console.log(game())
 
 
 
